8b89134c34f82f348c3b79040687508a670882f0
Rajiv Ranjan <51155706+rajivranjan89@users.noreply.github.com>
2021-06-17 19:23:15 +0530

    Create s3backend.tf
    S3 backend for storing state file remotely

---
+++ s3backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+    
+   
+  }
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
